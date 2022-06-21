790f92f84fc53102c32daa027e9e8c69305a5fdc
abdulaihalidu <abdulaihalidu008@gmail.com>
2022-06-21 18:14:22 +0300

    fixed mobile responsiveness problem

--- src/components/header/CallToAction.jsx
+++ src/components/header/CallToAction.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
-import CV from '../../assets/cv.pdf'
+import React from "react";
+import CV from "../../assets/my-cv.pdf";
 
 const CallToAction = () => {
   return (
-      <div className='cta'>
-          <a href={CV} download className='btn'>Download CV</a>
-          <a href='#contact' className='btn btn-primary'>Talk to me</a>
-      </div>
-  )
-}
+    <div className="cta">
+      <a href={CV} download className="btn">
+        Download CV
+      </a>
+      <a href="#contact" className="btn btn-primary">
+        Talk to me
+      </a>
+    </div>
+  );
+};
 
-export default CallToAction
+export default CallToAction;
