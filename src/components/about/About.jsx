9201ef36a0f1fef853906364ec0895d0ce7d7103
abdulaihalidu <abdulaihalidu008@gmail.com>
2022-06-21 19:12:49 +0300

    about page updated

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,15 +1,14 @@
-import React from 'react'
-import './about.css'
+import React from "react";
+import "./about.css";
 
-import myImage from '../../assets/halid.jpeg'
-import { FaAward } from 'react-icons/fa';
-import { FiUsers } from 'react-icons/fi';
-import { BsFolderCheck } from 'react-icons/bs';
-
+import myImage from "../../assets/halid.jpeg";
+import { FaAward } from "react-icons/fa";
+import { FiUsers } from "react-icons/fi";
+import { BsFolderCheck } from "react-icons/bs";
 
 const About = () => {
   return (
-    <section id='about'>
+    <section id="about">
       <h4>Explore More</h4>
       <h2>About Me</h2>
       <div className="container about__container">
@@ -20,37 +19,37 @@ const About = () => {
         </div>
         <div className="about__content">
           <div className="about__cards">
-            <article className='about__card'>
-              <FaAward className='about__card-icon'/>
+            <article className="about__card">
+              <FaAward className="about__card-icon" />
               <h4>Experience</h4>
               <small>1+ Years</small>
             </article>
 
-            <article className='about__card'>
-              <FiUsers className='about__card-icon'/>
+            <article className="about__card">
+              <FiUsers className="about__card-icon" />
               <h4>Clients</h4>
               <small>10+ clients</small>
             </article>
 
-            <article className='about__card'>
-              <BsFolderCheck className='about__card-icon'/>
+            <article className="about__card">
+              <BsFolderCheck className="about__card-icon" />
               <h4>Projects</h4>
               <small>10+ Completed Projects</small>
             </article>
-            
           </div>
           <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-            Neque tempora laborum dolor doloremque quibusdam,
-            iure molestiae placeat nostrum voluptate molestias
-            rerum fugiat vel quidem nemo, voluptatem, accusamus vero! Iusto,
-            consectetur!
+            A self-motivated, reliable Intern with research experience in
+            machine learning and web development seeking an internship
+            opportunity to expand skills and gain valuable real-world
+            experience.
           </p>
-          <a href='#contact' className='btn btn-primary'>Get In Touch</a>
+          <a href="#contact" className="btn btn-primary">
+            Get In Touch
+          </a>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default About
+export default About;
